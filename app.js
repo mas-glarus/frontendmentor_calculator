d97173fd8066f3dc30b9f86a397979b4558a6865
marc aschwanden <marc.aschwanden88@gmail.com>
2022-10-25 15:37:31 +0200

    create theme 1 desktop layout

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hi')
+const body = document.querySelector('body');
+const theme1 = document.getElementById('theme1');
+const theme2 = document.getElementById('theme2');
+const theme3 = document.getElementById('theme3');
+
+theme1.addEventListener('click', () => {
+    body.classList.remove('theme2', 'theme3')
+    body.classList.add('theme1')
+});
+
+theme2.addEventListener('click', () => {
+    body.classList.remove('theme1', 'theme3')
+    body.classList.add('theme2');
+})
+
+theme3.addEventListener('click', () => {
+    body.classList.remove('theme1', 'theme2')
+    body.classList.add('theme3')
+})
