6ca849214797dd262bb99f2e170f1ef19f17ecc7
marc aschwanden <marc.aschwanden88@gmail.com>
2022-10-25 20:58:54 +0200

    created three themes

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const body = document.querySelector('body');
-const theme1 = document.getElementById('theme1');
-const theme2 = document.getElementById('theme2');
-const theme3 = document.getElementById('theme3');
-
-theme1.addEventListener('click', () => {
-    body.classList.remove('theme2', 'theme3')
-    body.classList.add('theme1')
-});
-
-theme2.addEventListener('click', () => {
-    body.classList.remove('theme1', 'theme3')
-    body.classList.add('theme2');
-})
-
-theme3.addEventListener('click', () => {
-    body.classList.remove('theme1', 'theme2')
-    body.classList.add('theme3')
-})
+function swapStyle(sheet) {
+    document.getElementById('pagestyle').setAttribute('href', sheet);
+}
